cbcfafb8fdef4cba8fe825a56eee73f3cbc74b8d
Junior Reis <gilmaikjunior@gmail.com>
2024-02-11 18:25:24 -0300

    adicionando perguntas no javascript

--- script.js
+++ script.js
@@ -1,87 +1,17 @@
-let perguntasQuiz = [
-    {
-      pergunta:"Qual a capital do Brasil?",
-      respostas: {
-        a: 'Porto Velho',
-        b: 'São Paulo',
-        c: 'Brasilia'
-      },
-      respostaCorreta: 'c'
-    },
-    {
-      pergunta:"Quantos dias tem um ano?",
-      respostas:{
-        a:'362',
-        b:'365',
-        c:'487'
-      },
-      respostaCorreta: 'b'
-    }
-  ];
-  
-  let quizContainer = document.getElementById('quiz');
-  let resultadoContainer = document.getElementById('resultado');
-  let botaoEnviar = document.getElementById('enviar');
-  
-  generateQuiz(perguntasQuiz, quizContainer, resultadoContainer, botaoEnviar);
-  
-  function generateQuiz(pergunta, quizContainer, resultadoContainer, botaoEnviar){
-  
-    function mostrePerguntas(pergunta, quizContainer){
-      let saida = [];
-      let respostas;
-  
-      for(let i=0; i<pergunta.length; i++){
-        respostas = [];
-  
-        for(letra in pergunta[i].respostas){
-  
-  
-          respostas.push(
-            '<label>'
-              +'<input type="radio" name="pergunta'+i+'" value="'+letra+'">'
-              +pergunta[i].respostas[letra]
-              +'</label>'
-          );
-        }
-  
-        saida.push(
-          '<div class="pergunta">'+ pergunta[i].pergunta + '</div>' + '<div class="respostas">' + respostas.join('') + '</div>'
-        );
-      }
-  
-      quizContainer.innerHTML = saida.join('');
-    }
-  
-    function mostreResultados(pergunta, quizContainer, resultadoContainer){
-  
-      let respostaContainers = quizContainer.querySelectorAll('.respostas');
-  
-      let respostaUsuario = '';
-      let numCorreto = 0;
-  
-  
-        for(let i=0; i<pergunta.length; i++){
-  
-          respostaUsuario = (respostaContainers[i].querySelector('input[name=pergunta'+i+']:checked')||{}).value;
-  
-          if(respostaUsuario===pergunta[i].respostaCorreta){
-  
-            numCorreto++;
-  
-            respostaContainers[i].style.color = 'lightgreen';
-          }
-          else{
-            respostaContainers[i].style.color = 'red';
-          }
-        }
-  
-        resultadoContainer.innerHTML = numCorreto + ' de ' + pergunta.length;
-    }
-  
-    mostrePerguntas(pergunta, quizContainer);
-  
-    botaoEnviar.onclick = function(){
-      mostreResultados(pergunta, quizContainer, resultadoContainer)
-    }
+const perguntas = [
+  {
+    pergunta: "Como que todo código de HTML começa?",
+    alternativas: ["a) <head></head>", "b) <html></html>", "c) <!DOCTYPE html>", "d) <start></start>"],
+    iRespostaCorreta: 2
+  },
+  {
+    pergunta: "Qual tag que define o título de uma página em HTML?",
+    alternativas: ["a) <page></page>", "b) <name></name>", "c) <title></title>", "d) <p></p>"],
+    iRespostaCorreta: 2
+  },
+  {
+    pergunta: "Qual tag define um parágrafo em HTML?",
+    alternativas: ["a) <p></p>", "b) <text></text>", "c) <h1></h1>", "d) <page></page>"],
+    iRespostaCorreta: 0
   }
+]
