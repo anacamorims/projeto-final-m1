bab26e4a763cd93c7025759affc338fd87d93b3d
anacamorims <ana.camorims.dev@gmail.com>
2024-02-14 02:06:17 -0400

    add modification js

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ const perguntas = [
   },
   {
     pergunta: "Qual tag que define o título de uma página em HTML?",
-    alternativas: ["a) <page></page>", "b) <name></name>", "c) <title></title>", "d) <p></p>"],
+    alternativas: ["a)<page></page>", "b)<name></name>", "c)<title></title>", "d)<p></p>"],
     iRespostaCorreta: 2
   },
   {
     pergunta: "Qual tag define um parágrafo em HTML?",
-    alternativas: ["a) <p></p>", "b) <text></text>", "c) <h1></h1>", "d) <page></page>"],
+    alternativas: ["a)<p></p>", "b)<text></text>", "c)<h1></h1>", "d)<page></page>"],
     iRespostaCorreta: 0
+  },
+  {
+    pergunta: "Qual tag coloca uma foto no Website?",
+    alternativas: ["a)<photo>", "b)<src>", "c)<img>", "d)<media>"],
+    iRespostaCorreta: 2
   }
 ]
 
@@ -20,6 +25,7 @@ const perguntas = [
 const divQuiz = document.getElementById("quiz-container")
 const divPergunta = document.getElementById("question")
 const divAlternativas = document.getElementById("alternativas")
+const restartPageEl = document.getElementById("restart-page")
 
 let iPerguntaAtual = 0
 let pontuacao = 0
@@ -27,9 +33,17 @@ let pontuacao = 0
 
 function mostraPergunta() {
   const perguntaAtual = perguntas[iPerguntaAtual]
+  
+  // restart question section
+  divPergunta.style.display = 'block'
   divPergunta.innerHTML = ""
   divPergunta.textContent = perguntaAtual.pergunta
+
+  // restart alternatives section
+  divAlternativas.style.display = 'flex'
   divAlternativas.innerHTML = ""
+
+  hiddenRestartPage()
 
 //ou perguntaAtual.alternativas.length
   for (let i = 0; i <= 3; i++) {
@@ -52,16 +66,8 @@ function verificar(botaoApertado) {
   } else {
     alert("Vixi você errou, reinicie o quiz e tente de novo")
 
-    /* aqui*/
-    
-    if (!document.getElementById("btnReiniciar")) {
-      const botaoReiniciar = document.createElement("button");
-      botaoReiniciar.id = "btnReiniciar";
-      botaoReiniciar.textContent = "Reiniciar Quiz";
-      botaoReiniciar.addEventListener("click", reiniciarQuiz);
-      divAlternativas.appendChild(botaoReiniciar);
-  
-    }
+    showRestartPage()
+
     return;  
   }
   
@@ -69,7 +75,7 @@ function verificar(botaoApertado) {
   iPerguntaAtual++;
 
 //ou perguntas.length
-if (iPerguntaAtual < 3) {
+if (iPerguntaAtual < 4) {
   mostraPergunta()
 } else {
   exibirResultado()
@@ -90,6 +96,16 @@ function exibirResultado() {
   divQuiz.innerHTML = `<h2>Quiz concluído!</h2><p>Sua pontuação: ${pontuacao} de ${perguntas.length}</p>`;
 }
 
+function showRestartPage() {
+  document.getElementById("question").style.display = 'none'
+  document.getElementById("alternatives").style.display = 'none'
 
+  restartPageEl.style.display = 'flex'
+  restartPageEl.addEventListener("click", reiniciarQuiz);
+}
+
+function hiddenRestartPage() {
+  restartPageEl.style.display = 'none'
+}
 
 mostraPergunta()
